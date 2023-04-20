e734325df1301bb04846e7986f4368e15b888247
Bogdan <ovcharovbohdan43@gmail.com>
2023-04-20 11:44:27 +0100

    New commit

--- style.js
+++ style.js
@@ -1,7 +1,18 @@
-const obj = {
-	name: "Alex",
-	Age: 25,
-	isMarried: false
-};
+const numberOfFilms = +prompt("Сколько фильмов ты уже посмотрел?, "");
 
-console.log(obj.name);
+const personalMovieDB = [
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	ganers: [],
+	privat: false
+];
+
+const a = prompt("Один из последних просмотренных фильмов?"),
+	b = prompt("На сколько оцените его?"),
+	c = prompt("Один из последних просмотренных фильмов?"),
+	d = prompt("Один из последних просмотренных фильмов?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
